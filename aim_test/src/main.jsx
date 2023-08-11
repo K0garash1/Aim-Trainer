1282f80701ce75c76c4382ac6cc251c3c095b166
kogarashi <brayanarellano1605@gmail.com>
2023-08-11 17:53:03 -0500

    Refactor

--- aim_test/src/main.jsx
+++ aim_test/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AimTrainerApp from './AimTrainerApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AimTrainerApp />
   </React.StrictMode>,
 )
